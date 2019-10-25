d784d8cbf8f40e2571bb7209df1a44c9be737d57
exiledkingcc <exiledkingcc@gmail.com>
2019-10-25 10:29:15 +0800

    rename _w2bs and _bs2w

--- zcrypto/utils.h
+++ zcrypto/utils.h
@@ -12,11 +12,11 @@ static inline uint32_t _rleft(uint32_t x, int n) {
     return ( (x) << (n) ) | ( (x) >> (32 - n) );
 }
 
-static inline uint32_t _bs2w(const uint8_t bs[4]) {
+static inline uint32_t _load_u32(const uint8_t bs[4]) {
     return ((uint32_t)bs[0] << 24) | ((uint32_t)bs[1] << 16) | ((uint32_t)bs[2] << 8) | bs[3];
 }
 
-static inline void _w2bs(const uint32_t x, uint8_t bs[4]) {
+static inline void _store_u32(const uint32_t x, uint8_t bs[4]) {
     bs[0] = (x >> 24) & 0xff;
     bs[1] = (x >> 16) & 0xff;
     bs[2] = (x >> 8) & 0xff;
